9b4b271a79e069269df941f6b08f5e9f53036aa0
dracek <dracek@gmail.com>
2024-11-27 18:21:38 +0100

    sht

--- modify.py
+++ modify.py
@@ -2,11 +2,11 @@ import json
 
 # Načti JSON soubor
 with open('version.json', 'r') as file:
-data = json.load(file)
+    data = json.load(file)
 
-# Změň hodnotu
-data['version'] = '2.0.0'
+    # Změň hodnotu
+    data['version'] = '2.0.0'
 
 # Ulož zpět do JSON souboru
 with open('version.json', 'w') as file:
-json.dump(data, file, indent=2)
+    json.dump(data, file, indent=2)
